b6f82119dfec3779e081eb45c6c7a0388fd1fb16
Dan_Sparrow <d3186175@gmail.com>
2023-07-23 12:57:04 +0300

    дз занятие 5

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('app_advertisements.urls')),
+    path('top-sellers', include('app_advertisements.urls')),
     path('', include('app_lesson_4.urls')),
 ]
