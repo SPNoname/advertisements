a5c3f28b873e53562f2c4602508ec0468f47caad
SPNoname <136253023+SPNoname@users.noreply.github.com>
2023-08-06 15:24:57 +0300

    дз занятие 8

--- mysite/app_advertisements/models.py
+++ mysite/app_advertisements/models.py
@@ -1,11 +1,37 @@
 from django.db import models
-
+from django.contrib import admin
+from django.utils.html import format_html
 
 class Advertisement(models.Model):
     title = models.CharField("заголовок", max_length=60)
-    description = models. TextField("описание")
+    description = models.textfield("описание")
     price = models.DecimalField("цена", max_digits=10, decimal_places=2)
-    is_auction = models.BooleanField("уместен ли торг", help_text="отметьте, если уместен торг")
+    is_auction = models.BooleanField("уместен ли торг", help_text="отметьте, если торг по обьявлению уместен.")
     updated_at = models.DateTimeField("дата обновления", auto_now=True)
     created_at = models.DateTimeField("дата публикации", auto_now_add=True)
     
+    class Meta:
+        db_table = "advertisements"
+
+    def __str__(self) -> str:
+        return f"Advertisement(id={self.id}, title={self.title}, is_auction={self.is_auction})"
+
+    @admin.display(description="дата публикации")
+    def created_date(self):
+        from django.utils import timezone
+        if self.created_at.date() == timezone.now().date(): 
+            created_time = self.created_at.time().strftime("%H:%M")
+            return format_html(
+                "<span style='color: green; font-weight: bold'>сегодня в {} </span>",created_time
+            )
+        return self.created_at.strftime("%d:%m:%Y - %H:%M")
+    
+    @admin.display(description="дата обновления")
+    def updated_date(self):
+        from django.utils import timezone
+        if self.updated_at.date() == timezone.now().date(): 
+            updated_time = self.updated_at.time().strftime("%H:%M")
+            return format_html(
+                "<span style='color: blue; font-weight: bold'>сегодня в {} </span>",updated_time
+            )
+        return self.updated_at.strftime("%d:%m:%Y - %H:%M")
